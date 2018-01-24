572628ce2ddeca881fcee696d881c790dd608746
tom10987 <tom10987@gmail.com>
2018-01-25 00:00:32 +0900

    変数宣言

--- Syntax/src/main.rs
+++ Syntax/src/main.rs
@@ -1,6 +1,26 @@
 
 fn main()
 {
-    println!( "Hello world!" );
+    {
+        let x = 1;
+        println!( "x = {}", x );
+    }
+
+    {
+        let x: i32 = 2;
+        println!( "x = {}", x );
+    }
+
+    {
+        let x: i32;
+        x = 100;
+        println!( "x = {}", x );
+    }
+
+    {
+        let mut x = 100;
+        x = 200;
+        println!( "x = {}", x );
+    }
 }
 
